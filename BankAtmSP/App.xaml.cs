7293868469bfc4784a47c08df072ba5a0973e604
Nuriy_ff06 <Nuriy_ff06@STHQ0118-06>
2023-09-25 10:52:28 +0400

    Add project files.

---
+++ BankAtmSP/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BankAtmSP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Mutex _mutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "MyAppName";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                MessageBox.Show(@"App is already running
+You can not run the same app twice in same time ! ! !");
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
